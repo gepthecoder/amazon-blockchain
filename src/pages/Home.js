80c89fa9de90f1f73c3c67b8a7c359a033da4c7b
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-16 19:46:14 +0200

    $0008: [HomePage]: add home page style; implement carousel feature

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import "./Home.css";
+import { Carousel } from 'antd';
+
+import Carousel1 from "../images/carousel1.png";
+import Carousel2 from "../images/carousel2.png";
+import Carousel3 from "../images/carousel3.png";
+
+const carousel = [ Carousel1, Carousel2, Carousel3 ];
 
 const Home = () => {
 
@@ -9,8 +16,11 @@ return(
   <>
   <div className="container">
     <Header/>
-     <Link to="/product" className="link">Product Page</Link>
-     <Link to="/categories" className="link">Categories Page</Link>
+    <Carousel autoplay>
+      {carousel.map((e) => {
+        return <img src={e} className="carousel-img" alt="carousel"></img>;
+      })}
+    </Carousel>
   </div>
   </>
 )
