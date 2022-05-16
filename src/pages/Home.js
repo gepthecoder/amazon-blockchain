e806084cb970fd533b5bceea74851b5f7e436231
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-16 23:09:51 +0200

    $0009: [HomePage] [FeaturedCards]: create first comics category content-card + link to Comics

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import "./Home.css";
-import { Carousel } from 'antd';
+import { Carousel, Card } from 'antd';
 
 import Carousel1 from "../images/carousel1.png";
 import Carousel2 from "../images/carousel2.png";
 import Carousel3 from "../images/carousel3.png";
+
+import Comics from "../images/comics.png";
+import ArtemisFowl from "../images/ArtemisFowl.png";
+import MobyDick from "../images/MobyDick.png";
+import Adventure from "../images/adventure.png";
+import Dictionaries from "../images/dictionaries.png";
+import Fantasy from "../images/fantasy.png";
+import Horror from "../images/horror.png";
 
 const carousel = [ Carousel1, Carousel2, Carousel3 ];
 
@@ -21,6 +29,16 @@ return(
         return <img src={e} className="carousel-img" alt="carousel"></img>;
       })}
     </Carousel>
+    <div className="cards">
+      <Card className="card">
+        <h1>Browse Comics</h1>
+        <img src={Comics} alt="Comics Category" className="card-content"></img>
+        <br />
+        <Link to="/categories" state={"Comics"} className="link">
+          Shop Now
+        </Link>
+      </Card>
+    </div>
   </div>
   </>
 )
