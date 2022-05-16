c0c9942a78e9230f2c8cacc6720f37f200353d8f
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-16 18:16:29 +0200

    $0001: [Header]: add logo images and create style component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import { PageHeader, Button } from 'antd';
 import { useMoralis } from "react-moralis";
 import './Header.css'
+import Amazon from "../images/logo.png";
+import USA from "../images/usa.png";
+import BookStore from "../images/bookstore.png";
 
 const Header = () => {
   const { authenticate } = useMoralis();
@@ -8,11 +11,15 @@ const Header = () => {
     <div className="site-page-header-ghost-wrapper">
       <PageHeader
         ghost={false}
-        title="Amazon Clone"
         extra={[
-         <Button key="1" type="primary" onClick={() => authenticate()}>
-          Login with Metamask
-          </Button>
+          <>
+            <img src={Amazon} className="logo"></img>
+            <img src={BookStore} className="logo"></img>
+            <Button key="1" type="primary" onClick={() => authenticate()}>
+              Login with Metamask
+            </Button>
+          </>
+          
         ]}>
       </PageHeader>
     </div>
