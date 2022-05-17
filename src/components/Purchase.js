6a16c1205c82b0a030bbd7499d20ed2227de1389
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-17 12:59:14 +0200

    $0029: [ProductPage]: setup purchase details modal -  book price, purchase info (quantity, in-stock), buy now button part3

--- src/components/Purchase.js
+++ src/components/Purchase.js
@@ -1,6 +1,31 @@
+import {Select, Button} from 'antd'
+import {ShoppingCartOutlined} from "@ant-design/icons";
+
+const {Option} = Select;
+
+
 function Purchase({book}) {
   return (
-    <div>{book.name}</div>
+    <>
+        <span className="price"> ${book.price}</span>
+        <p>No Import Fees & Free Shipping Included</p>
+        <h1 style={{ color: "green" }}> In Stock </h1>
+        <h3>Quantity</h3>
+        <Select defaultValue={1} style={{ width: "100%" }}>
+            <Option value={1}>1</Option>
+            <Option value={2}>2</Option>
+            <Option value={3}>3</Option>
+            <Option value={4}>4</Option>
+            <Option value={5}>5</Option>
+        </Select>
+        <Button
+            className="login"
+            style={{ width: "100%", marginTop: "50px" }}
+            onClick={()=>console.log('🎈')}
+        >
+            <ShoppingCartOutlined /> Buy Now
+        </Button>
+  </>
   )
 }
 
