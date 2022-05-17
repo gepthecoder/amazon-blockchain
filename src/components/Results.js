d08a6cc762d05cb71f27bfd4a0d2710629c07d13
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-17 12:12:39 +0200

    $0024: [CategoriesPage] [ContentPage]: render filtered products

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,14 +1,39 @@
 import {books} from "../books.js";
-
+import {Card, Rate} from 'antd';
+import { Link } from 'react-router-dom';
+import "./Results.css";
 
 function Results({category, rating, priceMin, priceMax}) {
 
     // filter by rating -> by price (over Min) -> by price (under Max)
     const bookCategory = books[category].filter(x => x.rating >= rating).filter(x => x.price > priceMin).filter(x => x.price <= priceMax);
     console.log(bookCategory);
-    
+
     return (
-        <div>Results</div>
+        <>
+            {bookCategory.map((e,i) => {
+                return (
+                <Card>
+                <div style={{ display: "flex" }}>
+                    <img src={e.image} alt={i} width="300px"></img>
+                    <div>
+                    <p className="title">
+                        {e.name}
+                    </p>
+                    <Rate value={e.rating} disabled={true}></Rate>
+                    <h2> ${e.price}</h2>
+                    <p>
+                        Ships to Your Location
+                    </p>
+                    <Link to="/product" state={e} className="login">
+                    Got to Product Page
+                    </Link>
+                    </div>
+                </div>
+                </Card>
+                );
+            })}
+        </>
     )
 }
 
