aa59193cae9a9fbae1a0494e346646f79742c3e4
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-17 13:07:37 +0200

    $0030: [ProductPage]:  create a modal pop up (on Buy Now btn click) to finalize purchase part4

--- src/components/Purchase.js
+++ src/components/Purchase.js
@@ -1,32 +1,43 @@
-import {Select, Button} from 'antd'
+import {Select, Button, Modal} from 'antd'
 import {ShoppingCartOutlined} from "@ant-design/icons";
+import { useState } from 'react';
 
 const {Option} = Select;
 
 
 function Purchase({book}) {
-  return (
-    <>
-        <span className="price"> ${book.price}</span>
-        <p>No Import Fees & Free Shipping Included</p>
-        <h1 style={{ color: "green" }}> In Stock </h1>
-        <h3>Quantity</h3>
-        <Select defaultValue={1} style={{ width: "100%" }}>
-            <Option value={1}>1</Option>
-            <Option value={2}>2</Option>
-            <Option value={3}>3</Option>
-            <Option value={4}>4</Option>
-            <Option value={5}>5</Option>
-        </Select>
-        <Button
-            className="login"
-            style={{ width: "100%", marginTop: "50px" }}
-            onClick={()=>console.log('🎈')}
-        >
-            <ShoppingCartOutlined /> Buy Now
-        </Button>
-  </>
-  )
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    return (
+        <>
+            <span className="price"> ${book.price}</span>
+            <p>No Import Fees & Free Shipping Included</p>
+            <h1 style={{ color: "green" }}> In Stock </h1>
+            <h3>Quantity</h3>
+            <Select defaultValue={1} style={{ width: "100%" }}>
+                <Option value={1}>1</Option>
+                <Option value={2}>2</Option>
+                <Option value={3}>3</Option>
+                <Option value={4}>4</Option>
+                <Option value={5}>5</Option>
+            </Select>
+            <Button
+                className="login"
+                style={{ width: "100%", marginTop: "50px" }}
+                onClick={()=>setIsModalVisible(true)}
+            >
+                <ShoppingCartOutlined /> Buy Now
+            </Button>
+
+            <Modal
+                title="Purchase Product"
+                visible={isModalVisible}
+                /*onOk={handleOk}*/
+                onCancel={()=>setIsModalVisible(false)}
+            >
+            </Modal>
+        </>
+    )
 }
 
 export default Purchase
