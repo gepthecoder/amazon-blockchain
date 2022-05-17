de2fd571811eebb54edb2ca1d95b49f61df6c4b5
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-17 12:25:04 +0200

    $0025: [ProductPage]: retain metadata from selected products - display product image with hover effect + css

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Button } from 'antd';
+import Header from '../components/Header';
+import "./Product.css";
+import { useLocation } from 'react-router';
+
 
 const Product = () => {
   
+  let {state: book} = useLocation(); // tracks which product to display
+
   return (
-  <>
-  <div className="container">
-    <Button type="primary">Product Page</Button>
-  </div>
-  </>
-)
+    <>
+      <div className="container">
+        <Header />
+        <div className="product-content">
+          <div>
+            <div className="product-img">
+              <img src={book.image} alt="product" width="100%"></img>
+            </div>
+            <p style={{ textAlign: "center" }}>Hover over image to zoom</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Product;
