82873411b72097fa41abdc151f4d8ae29b20f47f
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-17 10:49:59 +0200

    $0013: [CategoriesPage]: create basic body layout - side bar; main content

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { Button } from 'antd';
 import { useLocation } from 'react-router-dom';
 import Header from '../components/Header';
+import { Layout } from 'antd';
 
 const Categories = () => {
+
+const { Sider, Content } = Layout;
 
 const {state: category} = useLocation()
 
@@ -15,6 +18,15 @@ return(
           <span>Showing Products For </span>
           <span className="category">"{category}"</span>
       </div>
+      <Layout>
+        <Sider width="340px" theme="dark" style={{ padding: "25px" }}></Sider>
+        <Content
+          theme="light"
+          style={{ padding: "35px", backgroundColor: "brown"}}
+        >
+        </Content>
+      </Layout>
+
       <Button 
         type="primary"
         onClick={() => console.log(category)}
