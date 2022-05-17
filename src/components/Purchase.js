6eb76c5c6934d3b01da9a4302822e7c8d1859d89
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-17 14:45:20 +0200

    $0040: [ProductPage] [PurchaseModal]: spacings - part7

--- src/components/Purchase.js
+++ src/components/Purchase.js
@@ -4,88 +4,85 @@ import { useState } from 'react';
 import { useMoralis } from 'react-moralis';
 
 const {Option} = Select;
-
 function Purchase({book}) {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [delivery, setDelivery] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [delivery, setDelivery] = useState("");
+  const {Moralis, account, chainId} = useMoralis();
 
-    const {Moralis, account, chainId} = useMoralis();
+  const handleOk = async () => {
 
+    // Get The Price of MATIC
 
-    const handleOk = async () => {
-        
-        // get current price of matic
-        const options = {
-            address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
-            chain: "eth"
-        };
-        const price = await Moralis.Web3API.token.getTokenPrice(options);
-        console.log("Current USD value of Matic " + price.usdPrice, '💸');
-        const priceMatic = book.price / price.usdPrice;
-        console.log("Book Price In Matic " + priceMatic, '💎');
-
-        // send matic to book store owner addresss
-        const options1 = {
-            type: "native", 
-            amount: Moralis.Units.ETH(priceMatic), 
-            receiver: "0xe276941FBd5f936E677dB9B6eEE8212a3b268C5E"
-        };
-
-        let result = await Moralis.transfer(options1);
-
-        // save transaction details to moralis db
-        const Transaction = Moralis.Object.extend("Transaction");
-        const transaction = new Transaction();
+    const options = {
+      address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
+      chain: "eth"
+    };
+    const price = await Moralis.Web3API.token.getTokenPrice(options);
+    const priceMatic = book.price / price.usdPrice;
     
-        transaction.set("Customer", account);
-        transaction.set("Delivery", delivery);
-        transaction.set("Product", book.name);
-    
-        transaction.save()
+    // Send Matic to book store owenr address
 
-        setIsModalVisible(false);
+    const options1 = {
+      type: "native", 
+      amount: Moralis.Units.ETH(priceMatic), 
+      receiver: "0xe276941FBd5f936E677dB9B6eEE8212a3b268C5E"
     }
+    let result = await Moralis.transfer(options1)
 
-    return (
-        <>
-            <span className="price"> ${book.price}</span>
-            <p>No Import Fees & Free Shipping Included</p>
-            <h1 style={{ color: "green" }}> In Stock </h1>
-            <h3>Quantity</h3>
-            <Select defaultValue={1} style={{ width: "100%" }}>
-                <Option value={1}>1</Option>
-                <Option value={2}>2</Option>
-                <Option value={3}>3</Option>
-                <Option value={4}>4</Option>
-                <Option value={5}>5</Option>
-            </Select>
-            {chainId === "0x13881" &&
-                <Button
-                    className="login"
-                    style={{ width: "100%", marginTop: "50px" }}
-                    onClick={()=>setIsModalVisible(true)}
-                >
-                    <ShoppingCartOutlined /> Buy Now
-                </Button>
-            }
-            <Modal
-                title="Purchase Product"
-                visible={isModalVisible}
-                onOk={handleOk}
-                onCancel={()=>setIsModalVisible(false)}
-            >
-                <div style={{ display: "flex" }}>
-                    <img src={book.image} alt="product" style={{ width: "200px" }}></img>
-                    <div>
-                        <h3>{book.name}</h3>
-                        <h2>${book.price}</h2>
-                        <h4>Delivery Address</h4>
-                        <Input onChange={(value) => setDelivery(value.target.value)}></Input>
-                    </div>
-                </div>
-            </Modal>
-        </>
-    )
+
+    //Save Transaction Details to DB
+    const Transaction = Moralis.Object.extend("Transaction");
+    const transaction = new Transaction();
+
+    transaction.set("Customer", account);
+    transaction.set("Delivery", delivery);
+    transaction.set("Product", book.name);
+
+    transaction.save()
+    setIsModalVisible(false);
+  }
+
+  return (
+    <>
+      <span className="price"> ${book.price}</span>
+      <p>No Import Fees & Free Shipping Included</p>
+      <h1 style={{ color: "green" }}> In Stock </h1>
+      <h3>Quantity</h3>
+      <Select defaultValue={1} style={{ width: "100%" }}>
+        <Option value={1}>1</Option>
+        <Option value={2}>2</Option>
+        <Option value={3}>3</Option>
+        <Option value={4}>4</Option>
+        <Option value={5}>5</Option>
+      </Select>
+      {chainId === "0x13881" &&
+      <Button
+      className="login"
+      style={{ width: "100%", marginTop: "50px" }}
+      onClick={()=>setIsModalVisible(true)}
+    >
+      <ShoppingCartOutlined /> Buy Now
+    </Button>
+      }
+      
+      <Modal
+        title="Purchase Product"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={()=>setIsModalVisible(false)}
+      >
+        <div style={{ display: "flex" }}>
+          <img src={book.image} alt="product" style={{ width: "200px" }}></img>
+          <div>
+            <h3>{book.name}</h3>
+            <h2>${book.price}</h2>
+            <h4>Delivery Address</h4>
+            <Input onChange={(value) => setDelivery(value.target.value)}></Input>
+          </div>
+        </div>
+      </Modal>
+    </>
+  )
 }
 
 export default Purchase
