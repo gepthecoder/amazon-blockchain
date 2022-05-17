8fbaea78b0ef843d85ce62fd62db588af3351367
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-17 10:42:13 +0200

    $0012: [CategoriesPage]: add header and a subheader for showing category we are in

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
 import { useLocation } from 'react-router-dom';
+import Header from '../components/Header';
 
 const Categories = () => {
 
@@ -9,6 +10,11 @@ const {state: category} = useLocation()
 return(
   <>
     <div className="container">
+      <Header />
+      <div className="results-header">
+          <span>Showing Products For </span>
+          <span className="category">"{category}"</span>
+      </div>
       <Button 
         type="primary"
         onClick={() => console.log(category)}
